14d35666dbfaa4f0597969d0cf55ea9b04c3d3ed
Jo_Jo <JohannesJobst@gmx.at>
2024-08-14 23:37:34 +0200

    Secound Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fatmans_Kitchen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
